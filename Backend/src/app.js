f7fcc90f6f98b7b1743ce9d8ef21148f22fc1c51
TridiziDev3 <tridizi.dev3@gmail.com>
2026-02-09 14:42:05 +0530

    changes done

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,13 +6,7 @@ const testRoutes = require("./routes/test.routes");
 const videoRoutes = require("./routes/video.routes");
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://your-frontend-domain.vercel.app" // 👈 ADD THIS
-  ],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
